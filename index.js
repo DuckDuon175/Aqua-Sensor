3cdef4cd86235c918fce22f5a8e20aee761f4a74
Duong <duongtoan1k99@gmail.com>
2025-04-25 15:35:01 +0900

    fix module path error

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-const homeController = require('../controllers/home.js')
+const homeController = require('./controllers/home.js')
 
 app.use(express.static('public'))
 app.get('/', homeController)
